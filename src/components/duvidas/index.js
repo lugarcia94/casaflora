5f1e3aa14514d948b8f8b519c4a14b7abf78a8ca
Matheus Sabes <matheussabes@MacBook-Pro-de-Matheus.local>
2021-02-15 09:02:18 -0300

    correcao

--- src/components/duvidas/index.js
+++ src/components/duvidas/index.js
@@ -1,11 +1,9 @@
 import './style.styl';
 
 $(".help__title").click(function () {
-    $('.help__title').parent().removeClass('help__item--open')
     $(this).parent().toggleClass("help__item--open");
 });
 
 $(".help__menu--item").click(function () {
-    $('.help__title').parent().removeClass('help__item--open')
     $(this).toggleClass("help__menu--open");
 });
